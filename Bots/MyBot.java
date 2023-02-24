3037e775f3f22003ee73f0c550808ded4ff280c5
Craig_ <21340633@studentmail.ul.ie>
2023-02-24 17:12:58 +0000

    Added some basic stuff to MyBot.java, needs to be cleaned up, some stuff will stay, rest will either be edited or deleted

--- Bots/MyBot.java
+++ Bots/MyBot.java
@@ -6,6 +6,7 @@ import java.awt.*;
 
 public class MyBot extends AdvancedRobot {
     private int moveDir = 1;
+    private double enemyNRG;
 
     public void run() {
         //Radar
@@ -26,26 +27,34 @@ public class MyBot extends AdvancedRobot {
 
     public void onScannedRobot(ScannedRobotEvent event) {
         double absBearing = event.getBearingRadians() + getHeadingRadians();//Enemy absolute bearing
+        double latVel=event.getVelocity() * Math.sin(event.getHeadingRadians() -absBearing);//enemies lateral Vel.
 
+        
         setTurnRadarLeftRadians(getRadarTurnRemainingRadians());//Radar lock
 
         //Arena wall detection
         if (getBattleFieldWidth() - getX() == 20 || getBattleFieldHeight() - getY() == 20) {
             //Alter direction
-            setAhead(100 * moveDir);
             moveDir *= -1;
+            setAhead(100 * moveDir);
 
             // always square off against our enemy
-            setTurnRight(event.getBearing() + 90);
+            setTurnLeft(-90-event.getBearing());
 
             // strafe by changing direction every 20 ticks
-            if (getTime() % 20 == 0) {
-                moveDir *= -1;
-                setAhead(150 * moveDir);
-
-
-            }
+        }
+        if (getTime() % 20 == 0) {
+            moveDir *= -1;
+            setAhead(150 * -moveDir);
         }
 
+        if (event.getDistance() == 30) {
+            moveDir = -moveDir;
+        }
+
+    }
+
+    public void onHitWall(HitWallEvent event) {
+        moveDir = -moveDir;
     }
 }
