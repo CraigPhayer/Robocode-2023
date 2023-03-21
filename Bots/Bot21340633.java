6d7158fb0ebdf85412603cc64af9c2a6a1c7e31a
Craig_ <21340633@studentmail.ul.ie>
2023-03-21 13:16:20 +0000

    Added fire control so my robot will kill its self less often

--- Bots/Bot21340633.java
+++ Bots/Bot21340633.java
@@ -4,14 +4,18 @@ import robocode.*;
 
 import static robocode.util.Utils.normalRelativeAngle;
 
-import java.awt.*;
+ import java.awt.*;
 
 public class Bot21340633 extends AdvancedRobot {
     private int direction = 1;
     private boolean win = true;
     private double enemyNRG;
+    private double POWER = 1.85;
+    private int hitCount = 0;
+    private int missCount = 0;
 
     public void run() {
+        //inactive();
         //Radar
         setAdjustRadarForRobotTurn(true);//keep the radar still while we turn
         turnRadarRightRadians(Double.POSITIVE_INFINITY);
@@ -70,7 +74,7 @@ public class Bot21340633 extends AdvancedRobot {
                 setTurnGunRightRadians(gun); //turn our gun
                 setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(absBearing - getHeadingRadians() + latVel / getVelocity()));//drive towards the enemies predicted future location
                 setAhead((event.getDistance() - 100) * direction);
-                setFire(3);
+                setFire(POWER);
 
             } else /*if (event.getDistance() <= 25 && getEnergy() > 85)*/ {//Too close to enemy
                 //moveDir = -moveDir;
@@ -78,23 +82,43 @@ public class Bot21340633 extends AdvancedRobot {
                 setTurnGunRightRadians(gun); //turn our gun
                 setTurnLeft(-90 - event.getBearing());
                 setAhead((event.getDistance() - 140) * direction);
-                setFire(3);
+                setFire(POWER);
             }
         }
     }
 
-
     public void inactive() {
-        while (true){
-            setTurnRight(10000);
-            setMaxVelocity(5);
-            ahead(10000);
+        while (true) {
+            if (getVelocity() == 0) {
+                setTurnRight(10000);
+                setMaxVelocity(5);
+                ahead(10000);
+            }
         }
 
     }
 
     public void onHitWall(HitWallEvent event) {
         direction *= -1;
+    }
+
+    public void onHitByBullet(HitByBulletEvent event) {
+
+    }
+
+    public void onHitRobot(HitRobotEvent event) {
+        if (event.isMyFault()) {
+            hitCount++;
+            missCount = 0;
+        } else if (missCount > 3)
+            POWER = .1;
+
+        if (hitCount >= 4) {
+            POWER = 3;
+            if (event.getEnergy() < 10) {
+                POWER = 2;
+            }
+        }
     }
 
     public void onWin(WinEvent event) {
